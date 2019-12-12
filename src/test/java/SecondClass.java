4b36e11abc1e90e2e05becca00d345bfda72b2e7
HEB3320 <55003460+HEB3320@users.noreply.github.com>
2019-12-12 16:57:11 -0500

    Update SecondClass.java

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,6 +1,7 @@
 public class SecondClass {
 
     public static void main(String[] args) {
+         System.out.println("Lois patron");
         System.out.println("Hi World");
         System.out.println("Second Class is Changed");
         System.out.println("Second Class is Changed again");
