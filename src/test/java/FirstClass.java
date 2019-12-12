35a12e2e829b55cabbc522f2276aaefb973d29bd
HEB3320 <55003460+HEB3320@users.noreply.github.com>
2019-12-12 15:37:00 -0500

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,11 +1,11 @@
 public class FirstClass {
 
     public static void main(String[] args) {
-
+        System.out.println("remote change");
         System.out.println("Hello Engin");
         System.out.println("I am working");
         System.out.println("I am working until 5.10pm");
         System.out.println("I am working until 10.10pm");
-        System.out.println("remote change");
+      
     }
 }
