80914b9141ec2034ebd5a1de2c31a597d30b0600
HEB3320 <55003460+HEB3320@users.noreply.github.com>
2019-12-12 15:24:29 -0500

    Update SecondClass.java

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -3,5 +3,6 @@ public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Hi World");
         System.out.println("Second Class is Changed");
+        System.out.println("Second Class is Changed again");
     }
 }
